9b205e7274660f1c781e2f266e868c6ea1df8cbc
iwtgh3525 <127456828+iwtgh3525@users.noreply.github.com>
2023-09-25 11:01:30 +0900

    v1

---
+++ Week04/print.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"strings"
+)
+
+func main() {
+
+	var a int = 9
+	var b = 2.7
+	var c, d string
+	var e float32 = 3.14
+	var f string
+	var g bool
+	h := 'Z'
+	i := "문자열"
+	J := "변수명이 대문자로 시작하면 다른 패키지에서 이 변수를 사용할 수 있음"
+	//koreanzombie := "정찬성"
+	koreanZombie := "정찬성" // Go 커뮤니티에서 제안하는 변수 표기법. Camel
+	fmt.Println(float64(a) > b)
+	fmt.Println(a * int(b))
+	//b = 2.7
+	//a = 9
+	d = "Go..."
+	b = 2.7
+	c = "inha~"
+	a = 9
+
+	fmt.Println(a, b, c, d, e, f, g, h, i, J, koreanZombie) // f, g는 zero value 출력
+	fmt.Println(reflect.TypeOf(h))                          // int32
+	fmt.Println(reflect.TypeOf(i))                          // string
+	fmt.Println(reflect.TypeOf(b))                          // float64
+
+	fmt.Println(a)
+	fmt.Println(reflect.TypeOf('B'))
+	fmt.Println(reflect.TypeOf(100))
+	fmt.Println(reflect.TypeOf(2.71))
+	fmt.Println(reflect.TypeOf(false))
+	fmt.Println(reflect.TypeOf("Go!"))
+	// fmt.Println(math.Floor("inha"))
+	// fmt.Println(strings.Title(3.14))
+	fmt.Println('A')
+	fmt.Println(math.Floor(2.15))
+	fmt.Println(math.Ceil(2.15))
+	fmt.Println('김', '\n')
+	fmt.Println(strings.Title("오픈소스\t프로그래밍~\n\"Go\""))
+	fmt.Println(strings.Title("open source programing go!"))
+}
---
+++ Week05/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var now time.Time = time.Now()
+	var year int = now.Year()
+	month := now.Month()
+	fmt.Println(year, month)
+	// fmt.Println(now.Month())
+	// fmt.Println(now.Hour(), now.Minute(), now.Second())
+}
